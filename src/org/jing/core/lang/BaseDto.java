84cf365a5a54ee3016c3db742c59f502becb5dbf
bksmate <357166513@qq.com>
2020-08-09 00:53:27 +0800

    1. 修正了BaseDto的toString无法得到继承关系的属性值的bug.

--- src/org/jing/core/lang/BaseDto.java
+++ src/org/jing/core/lang/BaseDto.java
@@ -1,6 +1,9 @@
 package org.jing.core.lang;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Description: 继承了该底层DTO的对象将会被重写以下方法: <br>
@@ -13,22 +16,28 @@ public abstract class BaseDto {
     @Override
     public String toString() {
         StringBuilder stbr = new StringBuilder("[");
-        stbr.append(getClass().getSimpleName()).append(": ");
-        Field[] fields = this.getClass().getDeclaredFields();
+        stbr.append(this.getClass().getSimpleName()).append(": ");
+        Class clazz = this.getClass();
+        List<Field> fields = new ArrayList<Field>();
+        while (clazz != null){
+            fields.addAll(Arrays.asList(clazz.getDeclaredFields()));
+            clazz = clazz.getSuperclass();
+        }
         boolean flag = false;
-        for (Field f$ : fields) {
+        for (Field field: fields) {
             if (flag) {
                 stbr.append(", ");
             }
-            f$.setAccessible(true);
-            stbr.append(f$.getName()).append("=");
-            try {
-                stbr.append(f$.get(this));
+            else {
+                flag = true;
             }
-            catch (Exception e) {
+            field.setAccessible(true);
+            stbr.append(field.getName()).append("=");
+            try {
+                stbr.append(field.get(this));
+            } catch (Exception e) {
                 stbr.append("null");
             }
-            flag = true;
         }
         stbr.append("]");
         return stbr.toString();
